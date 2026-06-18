0155da0f7b831edf46cddab93a9ee176c309f885
mizzz（ずーみー） <mizzz._.dev@outlook.jp>
2026-06-19 08:06:03 +0900

    Mod本体に報酬受け取りサービスを接続

--- mods/fabric/src/main/java/jp/ivrm/minecraft/activity/IvrmMinecraftActivityMod.java
+++ mods/fabric/src/main/java/jp/ivrm/minecraft/activity/IvrmMinecraftActivityMod.java
@@ -5,7 +5,9 @@ import jp.ivrm.minecraft.activity.command.IvrmActivityCommand;
 import jp.ivrm.minecraft.activity.config.ActivityConfig;
 import jp.ivrm.minecraft.activity.player.PlayerActivityTracker;
 import jp.ivrm.minecraft.activity.queue.LocalEventQueue;
+import jp.ivrm.minecraft.activity.reward.RewardClaimService;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.server.network.ServerPlayerEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,6 +19,7 @@ public final class IvrmMinecraftActivityMod implements ModInitializer {
     private LocalEventQueue queue;
     private ApiClient apiClient;
     private PlayerActivityTracker tracker;
+    private RewardClaimService rewardClaimService;
 
     @Override
     public void onInitialize() {
@@ -24,6 +27,7 @@ public final class IvrmMinecraftActivityMod implements ModInitializer {
         this.queue = new LocalEventQueue(config);
         this.apiClient = new ApiClient(config);
         this.tracker = new PlayerActivityTracker(config, apiClient, queue);
+        this.rewardClaimService = new RewardClaimService(config, apiClient);
 
         tracker.register();
         IvrmActivityCommand.register(this);
@@ -40,10 +44,19 @@ public final class IvrmMinecraftActivityMod implements ModInitializer {
         this.queue = new LocalEventQueue(config);
         this.apiClient = new ApiClient(config);
         this.tracker.updateRuntime(config, apiClient, queue);
+        this.rewardClaimService.updateRuntime(config, apiClient);
         LOGGER.info("IVRM Minecraft Activity config reloaded for server_id={}", config.server.id);
     }
 
     public void flushQueue() {
         queue.flush(apiClient);
+    }
+
+    public void listRewards(ServerPlayerEntity player) {
+        rewardClaimService.listPendingRewards(player);
+    }
+
+    public void claimRewards(ServerPlayerEntity player, boolean claimAll) {
+        rewardClaimService.claimRewards(player, claimAll);
     }
 }
